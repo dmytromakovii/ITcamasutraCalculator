93dde30e880d6349663cfc1d10e80165a5a9bafa
dmytromakovii <orinoko1989@gmail.com>
2021-02-07 11:29:29 +0200

    array of buttons has been added

--- script.js
+++ script.js
@@ -5,10 +5,6 @@ var buttonDivision = document.getElementById('buttonDivision');
 var inputResult = document.getElementById('result');
 var input1 = document.getElementById('number1');
 var input2 = document.getElementById('number2');
-buttonPlus.addEventListener('click', onOperationButtonClick);
-buttonMinus.addEventListener('click', onOperationButtonClick);
-buttonMultiply.addEventListener('click', onOperationButtonClick);
-buttonDivision.addEventListener('click', onOperationButtonClick);
 
 
 function makeOperation(operationType) {
@@ -35,104 +31,9 @@ function onOperationButtonClick(eventObject) {
 
 
 
+var arrayOfButtons = [buttonPlus, buttonMinus, buttonMultiply, buttonDivision]
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var buttonPlus = document.getElementById('buttonPlus');
-// var buttonMinus = document.getElementById('buttonMinus');
-// var buttonMultiply = document.getElementById('buttonMultiply');
-// var buttonDivision = document.getElementById('buttonDivision');
-
-// function onButtonPlusClick() {
-//     var input1 = document.getElementById('number1');
-//     var input2 = document.getElementById('number2');
-
-//     var number1 = Number(input1.value);
-//     var number2 = Number(input2.value);
-
-//     var sum = number1 + number2;
-
-//     console.log(sum);
-// }
-
-// function onButtonMinusClick() {
-//     var input1 = document.getElementById('number1');
-//     var input2 = document.getElementById('number2');
-
-//     var number1 = Number(input1.value);
-//     var number2 = Number(input2.value);
-
-//     var sum = number1 - number2;
-
-//     console.log(sum);
-// }
-
-// function onButtonMultiplyClick() {
-//     var input1 = document.getElementById('number1');
-//     var input2 = document.getElementById('number2');
-
-//     var number1 = Number(input1.value);
-//     var number2 = Number(input2.value);
-
-//     var sum = number1 * number2;
-
-//     console.log(sum);
-// }
-
-// function onButtonDivisionClick() {
-//     var input1 = document.getElementById('number1');
-//     var input2 = document.getElementById('number2');
-
-//     var number1 = Number(input1.value);
-//     var number2 = Number(input2.value);
-
-//     var sum = number1 / number2;
-
-//     console.log(sum);
-// }
-
-// buttonPlus.addEventListener('click', onButtonPlusClick);
-
-// buttonMinus.addEventListener('click', onButtonMinusClick);
-
-// buttonMultiply.addEventListener('click', onButtonMultiplyClick);
-
-// buttonDivision.addEventListener('click', onButtonDivisionClick);
+for (var i = 0; i < arrayOfButtons.length; i++) {
+    var button = arrayOfButtons[1]
+    arrayOfButtons[i].addEventListener('click', onOperationButtonClick);
+}
